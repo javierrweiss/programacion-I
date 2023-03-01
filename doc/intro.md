24646c202960fb0eac01ab081f9b26cdad8f134f
José Javier Blanco Rivero <javierweiss@gmail.com>
2023-03-01 19:48:17 -0300

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to javierweiss/programacion-I
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/javierweiss/programacion_I_test.clj
@@ -0,0 +1,7 @@
+(ns javierweiss.programacion-I-test
+  (:require [clojure.test :refer :all]
+            [javierweiss.programacion-I :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
